1ace61a2c82efb8dead289902c19fe4bd9a23c98
AdriánGarcíaLumbreras <adrianglumbreras@gmail.com>
2016-02-09 18:11:46 +0100

    Métodos de generación de clave para la carta del usuario

--- src/main/java/es/uniovi/asw/persistencia/Generador.java
+++ src/main/java/es/uniovi/asw/persistencia/Generador.java
@@ -15,8 +15,31 @@ public class Generador {
 	}
 	
 	private String generarPassword(Votante votante) {
-		// TODO Auto-generated method stub
-		return null;
+
+		String pass = votante.getNombre() + "." + votante.getNif();
+		
+		pass += generarValoresClave();
+		
+		return pass;
+	}
+
+	private String generarValoresClave() {
+
+		int i, valor;
+		char[] codigo = new char[5];
+		char[] elementos = {'0','1','2','3','4','5','6','7','8','9',
+						'a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+						'ñ','o','p','q','r','s','t','u','v','w','x','y','z',
+						'A','B','C','D','E','F','G','H','I','J','K','L','M','N',
+						'Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+						'*','+','.',',','ç','Ç'};
+		
+		for(i = 0; i < codigo.length; i++){
+			valor = (int)(Math.random() * elementos.length);
+			codigo[i] = (char)elementos[valor];
+		}
+		
+		return new String(codigo);
 	}
 
 	private String obtenerNombreFichero(Votante votante) {
@@ -27,5 +50,5 @@ public class Generador {
 	private String generarUsuario(Votante votante) {
 		
 		return votante.getNombre() + "." + votante.getEmail();
-	}
+	}	
 }

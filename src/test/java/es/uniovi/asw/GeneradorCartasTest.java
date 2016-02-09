7e43b9580869eb15f1c02b749a96ea1586945d2f
AdriánGarcíaLumbreras <adrianglumbreras@gmail.com>
2016-02-09 18:41:53 +0100

    Test Cartas

--- src/test/java/es/uniovi/asw/GeneradorCartasTest.java
+++ src/test/java/es/uniovi/asw/GeneradorCartasTest.java
@@ -1,16 +1,24 @@
 package test.java.es.uniovi.asw;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
+import main.java.es.uniovi.asw.logica.Votante;
+import main.java.es.uniovi.asw.persistencia.Generador;
+
+/**
+ * Clase para comprobar el funcionamiento del generador de cartas
+ * @author Agl
+ *
+ */
 public class GeneradorCartasTest {
 
-	@Test
-	public void crearVotante() {
-			
-	}
-	
+	private Votante votante = new Votante("Lumbreras", "lumbreras@email.com", "123456789A", 10, 5);
+		
 	@Test
 	public void crearCartas() {
+		
 		
 	}
 }

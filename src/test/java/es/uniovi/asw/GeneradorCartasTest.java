a97789d9a216a6845289d761805917773853f333
MonicaCueva <mcglezoviedo@outlook.es>
2016-02-10 17:31:02 +0100

    Cambios prueba

--- src/test/java/es/uniovi/asw/GeneradorCartasTest.java
+++ src/test/java/es/uniovi/asw/GeneradorCartasTest.java
@@ -15,12 +15,14 @@ public class GeneradorCartasTest {
 
 	private Votante votante = new Votante("Lumbreras", "lumbreras@email.com", "123456789A", 10, 5);
 	private Votante votante2 = new Votante("Monica", "monica@email.com", "125456789A", 12, 5);
+	private Votante votante3 = new Votante("Ana", "ana@email.com", "145456789A", 12, 5);
 
 	@Test
 	public void crearCartas() {
 				
 		assertEquals("Lumbreras", votante.getNombre());
 		assertEquals("Monica", votante2.getNombre());
+		assertEquals("Ana", votante3.getNombre());
 	}
 	
 }

9e0569548e28ed3326e5f789c1fc50fa0f3500e4
Ana <anabravo25@hotmail.com>
2016-02-13 12:25:14 +0100

    cambios LeerFichero

--- src/main/java/es/uniovi/asw/parser/LeerFichero.java
+++ src/main/java/es/uniovi/asw/parser/LeerFichero.java
@@ -2,12 +2,15 @@ package es.uniovi.asw.parser;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import es.uniovi.asw.persistencia.GestionBD;
 
 public class LeerFichero {
 		public static void readXLSXFile() throws IOException
@@ -22,29 +25,49 @@ public class LeerFichero {
 		XSSFCell cell;
 
 		Iterator rows = sheet.rowIterator();
-
+		ArrayList<ArrayList<Object>> array= new ArrayList<ArrayList<Object>>();
+		GestionBD g = new GestionBD();
+		
+		
 		while (rows.hasNext())
 		{
 			row=(XSSFRow) rows.next();
 			Iterator cells = row.cellIterator();
+			ArrayList<Object> datos= new ArrayList<Object>();
 			while (cells.hasNext())
 			{
 				cell=(XSSFCell) cells.next();
 		
 				if (cell.getCellType() == XSSFCell.CELL_TYPE_STRING)
 				{
-					System.out.print(cell.getStringCellValue()+" ");
+					//System.out.print(cell.getStringCellValue()+" ");
+					datos.add(cell.getStringCellValue());
 				}
 				else if(cell.getCellType() == XSSFCell.CELL_TYPE_NUMERIC)
 				{
-					System.out.print(cell.getNumericCellValue()+" ");
+					//System.out.print(cell.getNumericCellValue()+" ");
+					datos.add(cell.getNumericCellValue());
 				}
 				else
 				{
-					//U Can Handel Boolean, Formula, Errors
+					System.out.println("No está especificado el tipo");
 				}
+				
+			}
+			//System.out.println();
+			array.add(datos);
+
+		}
+		for(int i=0;i<array.size();i++){
+			for(int j=0;j<array.get(i).size();j++){
+				System.out.print(array.get(i).get(j)+"\t");
+				
 			}
 			System.out.println();
+			g.guardarVotanteDatosCompletos(String.valueOf(array.get(i).get(0)), String.valueOf(array.get(i).get(2)), 
+					String.valueOf(array.get(i).get(1)), Integer.parseInt(String.valueOf(array.get(i).get(3))), "", "");
+			
+		
 		}
 	
 	}
